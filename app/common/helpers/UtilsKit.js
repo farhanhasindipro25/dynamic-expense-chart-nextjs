94128cce3b7b69c083a73b670bcd9a7be037f85a
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2023-11-21 03:00:19 +0600

    Made the util function more generalized

--- app/common/helpers/UtilsKit.js
+++ app/common/helpers/UtilsKit.js
@@ -1,12 +1,26 @@
-
-export default function getSegmentPercentagePerTotal(data, period, segment) {
+export default function getSegmentPercentagePerTotal(data, period) {
   const targetPeriod = data.find((item) => item.period === period);
-  const targetSegment = targetPeriod[segment];
-  const totalOfPeriod =
-    targetPeriod.personal +
-    targetPeriod.shopping +
-    targetPeriod.phone +
-    targetPeriod.other;
-  const percentage = (targetSegment / totalOfPeriod) * 100;
-  return { totalOfPeriod, percentage };
+
+  if (!targetPeriod) {
+    // Returning an empty object if no period is found
+    return {};
+  }
+
+  const totalOfPeriod = Object.values(targetPeriod).reduce(
+    (acc, value) => acc + (isNaN(value) ? 0 : value),
+    0
+  );
+
+  const percentages = {};
+
+  Object.keys(targetPeriod).forEach((segment) => {
+    if (segment !== "period") {
+      const targetSegment = targetPeriod[segment];
+      const percentage = (targetSegment / totalOfPeriod) * 100;
+
+      percentages[segment] = isNaN(percentage) ? 0 : percentage;
+    }
+  });
+
+  return { totalOfPeriod, percentages };
 }
--- app/components/Chart.jsx
+++ app/components/Chart.jsx
@@ -1,17 +1,16 @@
-import calculateSubCategoryTotal, {
-  getSegmentPercentagePerTotal,
-} from "../common/helpers/UtilsKit";
+import getSegmentPercentagePerTotal from "../common/helpers/UtilsKit";
 
 export default function Chart({ data }) {
-  const { totalOfPeriod, percentage } = getSegmentPercentagePerTotal(
+  const { totalOfPeriod, percentages } = getSegmentPercentagePerTotal(
     data,
-    "1M",
-    "personal"
+    "1M"
   );
+
+  console.log(totalOfPeriod, percentages);
   return (
     <div className="border-8 rounded-full border-primary">
       <div className="rounded-full w-full h-full p-44 bg-gray-100">
-        {totalOfPeriod}
+        {/* {percentage} */}
       </div>
     </div>
   );
