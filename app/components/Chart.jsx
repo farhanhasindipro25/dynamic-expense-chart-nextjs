2d3b5c26833d87c0b7bf2919c9592ebf1e54d740
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2023-11-21 03:03:59 +0600

    Calculated all portion percentages and totals of each category

--- app/components/Chart.jsx
+++ app/components/Chart.jsx
@@ -1,12 +1,19 @@
 import getSegmentPercentagePerTotal from "../common/helpers/UtilsKit";
 
 export default function Chart({ data }) {
-  const { totalOfPeriod, percentages } = getSegmentPercentagePerTotal(
-    data,
-    "1M"
-  );
+  const { totalOfPeriod: total_1M, percentages: percentages_1M } =
+    getSegmentPercentagePerTotal(data, "1M");
+  const { totalOfPeriod: total_6M, percentages: percentages_6M } =
+    getSegmentPercentagePerTotal(data, "6M");
+  const { totalOfPeriod: total_1Y, percentages: percentages_1Y } =
+    getSegmentPercentagePerTotal(data, "1Y");
+  const { totalOfPeriod: total_All, percentages: percentages_All } =
+    getSegmentPercentagePerTotal(data, "ALL TIME");
 
-  console.log(totalOfPeriod, percentages);
+  console.log(total_1M, percentages_1M);
+  console.log(total_6M, percentages_6M);
+  console.log(total_1Y, percentages_1Y);
+  console.log(total_All, percentages_All);
   return (
     <div className="border-8 rounded-full border-primary">
       <div className="rounded-full w-full h-full p-44 bg-gray-100">
--- app/page.js
+++ app/page.js
@@ -2,28 +2,28 @@ import Chart from "./components/Chart";
 
 const EXPENSE_DATA = [
   {
-    period: "1M",
+    period: "1M", // 150 + 90 + 60 + 80 = 380
     personal: 150,
     shopping: 90,
     phone: 60,
-    other: 80, // 150 + 90 + 60 + 80 = 380
+    other: 80,
   },
   {
-    period: "6M",
+    period: "6M", // 320 + 240 + 255 + 298 = 1113
     personal: 320,
     shopping: 240,
     phone: 255,
-    other: 298, // 
+    other: 298,
   },
   {
-    period: "1Y",
+    period: "1Y", // 950 + 930 + 738 + 490 = 3108
     personal: 950,
     shopping: 930,
     phone: 738,
     other: 490,
   },
   {
-    period: "ALL TIME",
+    period: "ALL TIME", // 1800 + 1420 + 1265 + 1000 = 5485
     personal: 1800,
     shopping: 1420,
     phone: 1265,
