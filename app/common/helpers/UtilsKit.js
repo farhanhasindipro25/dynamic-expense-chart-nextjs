bfd2592c386e714600c91b886a12fc4d48e37f14
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2023-11-21 02:48:46 +0600

    Updated util function to calculate total of a category and the percentage of the portions per category

--- app/common/helpers/UtilsKit.js
+++ app/common/helpers/UtilsKit.js
@@ -1,9 +1,12 @@
-export default function calculatePercentageFromTotal(data, period) {
+
+export default function getSegmentPercentagePerTotal(data, period, segment) {
   const targetPeriod = data.find((item) => item.period === period);
+  const targetSegment = targetPeriod[segment];
   const totalOfPeriod =
     targetPeriod.personal +
     targetPeriod.shopping +
     targetPeriod.phone +
     targetPeriod.other;
-  return totalOfPeriod;
+  const percentage = (targetSegment / totalOfPeriod) * 100;
+  return { totalOfPeriod, percentage };
 }
--- app/components/Chart.jsx
+++ app/components/Chart.jsx
@@ -1,14 +1,17 @@
-import calculatePercentageFrom1MSegment from "../common/helpers/UtilsKit";
+import calculateSubCategoryTotal, {
+  getSegmentPercentagePerTotal,
+} from "../common/helpers/UtilsKit";
 
 export default function Chart({ data }) {
-  const calculate1MData = () => {};
-  const calculate6MData = () => {};
-  const calculate1YData = () => {};
-  const calculateAllTimeData = () => {};
+  const { totalOfPeriod, percentage } = getSegmentPercentagePerTotal(
+    data,
+    "1M",
+    "personal"
+  );
   return (
     <div className="border-8 rounded-full border-primary">
       <div className="rounded-full w-full h-full p-44 bg-gray-100">
-        {calculatePercentageFrom1MSegment(data, "1M")}
+        {totalOfPeriod}
       </div>
     </div>
   );
