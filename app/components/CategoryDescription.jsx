b4c52ac450f7365d0dc6a19d819b3b6b6ad71152
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2023-11-22 00:31:10 +0600

    Added category description component

--- app/components/CategoryDescription.jsx
+++ app/components/CategoryDescription.jsx
@@ -2,10 +2,22 @@ import Badge from "./Badge";
 
 export default function CategoryDescription() {
   return (
-    <div>
-      <div>
+    <div className="w-full flex justify-evenly">
+      <div className="flex items-center gap-4">
         <Badge variant="primary" />
-        <h2>Personal</h2>
+        <h2 className="text-gray-500 font-semibold text-lg">Personal</h2>
+      </div>
+      <div className="flex items-center gap-4">
+        <Badge variant="tertiary" />
+        <h2 className="text-gray-500 font-semibold text-lg">Shopping</h2>
+      </div>
+      <div className="flex items-center gap-4">
+        <Badge variant="accent" />
+        <h2 className="text-gray-500 font-semibold text-lg">Phone</h2>
+      </div>
+      <div className="flex items-center gap-4">
+        <Badge variant="secondary" />
+        <h2 className="text-gray-500 font-semibold text-lg">Other</h2>
       </div>
     </div>
   );
