49909904702540a657383ed89042dd61f1e34848
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2023-11-22 02:11:05 +0600

    Fixed styling of the tab navigation for filtering time periods

--- app/components/TabNavigation.jsx
+++ app/components/TabNavigation.jsx
@@ -5,14 +5,14 @@ import { cn } from "../common/helpers/UtilsKit";
 const DEFAULT_STYLES =
   "flex justify-center items-center cursor-pointer text-base truncate px-2";
 const ACTIVE_TAB_STYLES =
-  "font-semibold text-primary-600 rounded-t-md border-b-2 border-primary-600 px-2 pb-2";
-const DEFAULT_TAB_STYLES = "font-normal text-neutral-500 pb-2";
+  "font-semibold rounded-full text-primary bg-gray-200 p-3";
+const DEFAULT_TAB_STYLES = "font-medium text-gray-500";
 const SELECT_FIELD_STYLES =
   "appearance-none block w-full text-sm rounded-md text-neutral-500 border border-neutral-300 px-3 py-3 placeholder-neutral-400 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 active:border-primary-500 cursor-text";
 
 export default function TabNavigation({ tabs, current, setTab }) {
   return (
-    <ul className="flex w-full gap-8 border-b border-neutral-300">
+    <ul className="flex justify-center py-4 px-10 rounded-full gap-8 bg-gray-100">
       {tabs?.map((tab) => (
         <li
           key={tab.name}
